2222e656540d12660ac0b1063cc823fe8ada8359
Shang <yopshang@gmail.com>
2022-04-12 21:17:02 +0800

    schema/ model

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const http = require('http');
 const mongoose = require('mongoose');
 
 // 建立 Schema
+const schema = {
+    name: String,
+    price: {
+        type: Number,
+        default: 3000,
+        require: [true, '價格必填']
+    },
+    rating: Number
+}
 
 // 建立 Model
+const Room = mongoose.model('Room', schema);
+
 
 // 連線資料庫
 mongoose.connect('mongodb://localhost:27017/hotel').then(()=>{
