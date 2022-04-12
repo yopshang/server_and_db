64c13968e35a89a08efa90b71d103c14f8fd55ef
Shang <yopshang@gmail.com>
2022-04-12 21:45:43 +0800

    post 寫法糾正

--- postdata.js
+++ postdata.js
@@ -3,35 +3,37 @@ const mongoose = require('mongoose');
 
 // 建立 Schema
 const roomschema = {
-  name: String,
-  price: {
-    type: Number,
-    default: 3000
-  },
-  rating: {
-    type: Number,
-    default: 4.0
-  }
+    name: String,
+    price: {
+        type: Number,
+        default: 3000
+    },
+    rating: {
+        type: Number,
+        default: 4.0
+    }
 }
 // 建立 Model
 const user = mongoose.model('rooms', roomschema);
 
 
 
-function postdata(res ,header, msg, body){
-  res.writeHead(200, header);
-  const title = JSON.parse(body).title;
-  try{
-    mongoose.connect('mongodb://localhost:27017/hotel')
-      .then(() => {
-        let user_data = new user({name:title})
-        user_data.save()
-      }).then(()=>{
-        res.write('新增成功');
-      })
-  }catch(err){
-    console.log(err);
-  }
-  res.end();
+function postdata(res, header, msg, body) {
+    res.writeHead(200, header);
+    console.log(res)
+    
+    const title = JSON.parse(body).title;
+    try {
+        mongoose.connect('mongodb://localhost:27017/hotel')
+            .then(() => {
+                let user_data = new user({ name: title })
+                user_data.save()
+            }).then(() => {
+                res.write('新增成功');
+            })
+    } catch (err) {
+        console.log(err);
+    }
+    res.end();
 }
 module.exports = postdata;
--- server.js
+++ server.js
@@ -27,16 +27,16 @@ const Room = mongoose.model('Room', schema);
 // });
 
 
-// 建立 headers
-const headers = {
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-Width',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATH, POST, GET, OPTIONS, DELETE',
-    'Contnet-Type': 'application/json'
-}
-let body = "";
 
 const requestListener = (req, res) => {
+    // 建立 headers
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-Width',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATH, POST, GET, OPTIONS, DELETE',
+        'Contnet-Type': 'application/json'
+    }
+    let body = "";
     req.on('data', chunk => {
         body += chunk;
     })
@@ -44,16 +44,19 @@ const requestListener = (req, res) => {
     if (req.method == 'GET') {
         // getdata(res, header, '成功');
         res.writeHead(200, headers);
-
         res.write("取得資料");
         res.end()
-    } else if (res.method == 'POST') {
-        res.on('end', () => {
-            postdata(res, header, '更新',body)
+    } else if (req.method == 'POST') {
+        console.log('post',)
+        req.on('end', () => {
+            res.writeHead(200, headers);
+            res.write("post資料");
+            res.end()
+            // postdata(res, headers, '更新成功',body)
         })
     }
 
-  
+
 }
 const server = http.createServer(requestListener)
 server.listen(3005)
