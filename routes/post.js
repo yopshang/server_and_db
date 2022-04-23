7a963e118980b29ae4b493b6b36bc5d4722a3ca5
Shang <yopshang@gmail.com>
2022-04-23 10:07:02 +0800

    新增：express route

---
+++ routes/post.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/post', (req, res) => {
+    res.send();
+})
+router.get('room', (req, res) => {
+    res.send();
+})
+
+module.exports = router;
