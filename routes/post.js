7c2e43d22ab0f3e1289844ea223b9d501e55329a
Shang <yopshang@gmail.com>
2022-04-26 21:17:08 +0800

    新增router

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
 
-router.get('/post', (req, res) => {
-    res.send();
+const getdata =  require('../getdata');
+const postdata = require('../postdata');
+const {deletedata, deleteAllData} = require('../deletedata');
+const patchdata = require('../patchdata');
+
+async function returnBody(req){
+    let body = "";
+
+    await req.on('data', chunk=>{
+        body+=chunk;
+    })
+    return body;
+}
+
+router.get('/', (req, res) => {
+    getdata(res);
 })
-router.get('room', (req, res) => {
-    res.send();
+router.post('/', async (req, res) => {
+    let body = await returnBody(req);
+    postdata(res,body);
+})
+router.delete('/:id', async (req, res) => {
+    deletedata(req, res);
+})
+router.delete('', async (req, res) => {
+    deleteAllData(req, res);
+})
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = await returnBody(req);
+    patchdata(res, id , body);
+})
+router.options('/', async(req,res) => {
+    res.status(200).json({'status': 'success'})
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 
-const getdata =  require('./getdata');
-const postdata = require('./postdata');
-const {deletedata, deleteAllData} = require('./deletedata');
-const patchdata = require('./patchdata');
-
+const post = require('./routes/post')
 
 dotenv.config({path:"./config.env"})
 const dburl = process.env.DBURL;
@@ -24,73 +20,6 @@ mongoose.connect(db_varified)
         console.log(err);
     })
 
-        // const headers = {
-        //     'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-Width',
-        //     'Access-Control-Allow-Origin': '*',
-        //     'Access-Control-Allow-Methods': 'PATH, POST, GET, OPTIONS, DELETE',
-        //     'Contnet-Type': 'application/json'
-        // }
-// const requestListener =  async (req, res) => {
-//     // 建立 headers
-//     let body = "";
-    
-//     req.on('data', chunk=>{
-//         body+=chunk;
-//     })
 
-//     if (req.url == '/rooms' && req.method == 'GET') {
-//         getdata(res, headers);
-//     } else if ( req.url =='/rooms' && req.method == 'POST') {
-//         req.on('end', () => {
-//             postdata(res, headers, '更新成功',body)
-//         })
-//     }else if(req.url.startsWith('/rooms/') && req.method == 'DELETE'){
-//         const id = req.url.split('/').pop();
-//         console.log(id);
-//         await Post.findByIdAndDelete(id);
-//         console.log('刪除')
-//         res.writeHead(200, headers);
-//         res.write(JSON.stringify({
-//             "status": "success",
-//             "data": null
-//         }))
-//         res.end();
-//     }
-
-
-// }
-
-async function returnBody(req){
-    let body = "";
-
-    await req.on('data', chunk=>{
-        body+=chunk;
-    })
-    return body;
-}
-
-
-app.get('/posts', (req, res) => {
-    getdata(res);
-})
-app.post('/posts', async (req, res) => {
-    let body = await returnBody(req);
-    postdata(res,body);
-})
-app.delete('/posts/:id', async (req, res) => {
-    deletedata(req, res);
-})
-app.delete('/posts', async (req, res) => {
-    deleteAllData(req, res);
-})
-app.patch('/posts/:id', async (req, res) => {
-    const id = req.params.id;
-    const body = await returnBody(req);
-    patchdata(res, id , body);
-})
-app.options('/posts', async(req,res) => {
-    res.status(200).json({'status': 'success'})
-})
-
-
+app.use('/posts', post)
 app.listen(process.env.PORT || 3005)
