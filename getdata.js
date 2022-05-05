2dcac90609229496894fb76aab8a0f39ec35c41a
Shang <yopshang@gmail.com>
2022-05-05 21:31:37 +0800

    get 修正

--- getdata.js
+++ getdata.js
@@ -3,10 +3,10 @@ const Post = require('./models/post');
 async function getdata(res) {
     try {
         const post = await Post.find({});
-        res.status(200).json(JSON.stringify({
+        res.status(200).json({
             "status": "success",
             post
-        }));
+        });
     } catch (err) {
         console.log(err);
         res.json('資料取得錯誤');
